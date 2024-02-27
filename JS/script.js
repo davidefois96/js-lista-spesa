964fc45546f597eaa96724b397464f549bd49625
davidefois96 <davidefois360@gmail.com>
2024-02-27 15:28:07 +0100

    [add] new code

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,34 @@
 
 const list = [];
 
-const numProduct = parseInt(prompt('Inserisci numericamente quanti prodotti acquistare?'));
-
-ol = document.querySelector('ol');
+ul = document.querySelector('ul');
 
 let i = 0;
 
-while (list.length<numProduct) {
+const numProduct = parseInt(prompt('Inserisci numericamente quanti prodotti acquistare?'));
 
-  list[i] = prompt('inserisci un articolo!');
+if (!isNaN(numProduct)) {
+
+  while (list.length<numProduct) {
+
+    list[i] = prompt('inserisci un articolo!');
+    
+    ul.innerHTML += ` <li> ${list[i]} </li>`;
   
-  ol.innerHTML += ` <li> ${list[i]} </li>`;
+    i++;
+  
+    
+  }
 
-  i++;
 
+  
+} else {
+
+ alert('A LETTO SENZA CENA! LEGGI MEGLIO LA PROSSIMA VOLTA!')
+
+  ul.innerHTML += ` <li> Il nulla cosmico </li>`;
   
 }
+
+
+
